d36efc60f3c0c7d6a7b3fb2644749c0b21a14ff1
Benno <bennington.schaab@gmail.com>
2019-11-15 13:51:01 +0100

    CMmake

---
+++ cmake-build-debug/CMakeFiles/SchiffeVersenken.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SchiffeVersenken.dir/main.c.obj"
+  "SchiffeVersenken.exe"
+  "SchiffeVersenken.exe.manifest"
+  "SchiffeVersenken.pdb"
+  "libSchiffeVersenken.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SchiffeVersenken.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
