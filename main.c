7edd46b6a7b731622f5847e0fad44ff83807833b
Noah <noahboeckmann@gmx.net>
2019-11-17 22:31:37 +0100

    Die Matrizen für die beiden Spieler werden erstellt und initalisiert, Die Matrize des Spieler 0 wird mit der neuen drawScreen Methode gezeichnet (Hierfür wurde auch eine clearScreen Methode implementiert). Anmerkungen hinzugefügt

--- main.c
+++ main.c
@@ -1,20 +1,55 @@
 #include <stdio.h>
 
-int init();
+/*Rund um das Spielfeld und seine Erstellung*/
 int getBattlegroundSize();
+void initBattleground(char (*battleground0)[], char (*battleground1)[], int size);
+
+/*Alles, was das Spiel in die Konsole bringt*/
 void drawIntro();
-void drawScreen(char (*matrix)[]);
+void drawScreen(char (*matrix)[], int size, int playerTurn);
+void clearScreen();
 
 
 int main(void) {
     drawIntro();
-    int x = getBattlegroundSize();
-    char battleground[x][x];
+    int playerTurn = 0;
+
+    int size = getBattlegroundSize();
+    char battleground0[size][size]; /*Matrize Spieler 0*/
+    char battleground1[size][size]; /*Matrize Spieler 1*/
+
+    initBattleground(battleground0, battleground1, size);
+
+    drawScreen(battleground0, size, playerTurn);
+
+
     return 0;
 }
 
-int init() /*Initalisiert die Daten TODO: was auch immer erledigt werden muss */
+int getBattlegroundSize() /*Fragt den Spiele nach der gewünschten Feldgröße TODO: Alles andere als Zahlen abfangen*/
 {
+    int n;
+    do{
+        printf("Waehlen sie eine Feldgroesse: (min 10, max 20): ");
+        scanf("%i", &n); // NOLINT(cert-err34-c)
+        if (n < 10 || n > 20){
+            printf("Keine gueltige Eingabe!\n");
+            n = 0;
+        } else {
+            return n;
+        }
+    } while (!n);
+}
+
+void initBattleground(char (*battleground0)[], char (*battleground1)[], int size) /*Initalisiert die Matrizen mit '~' als leere Felder*/
+{
+    int x, y;
+    for (x = 0; x < size; x++){
+        for(y = 0; y < size; y++){
+            (*battleground0 + x)[y] = '~';
+            (*battleground1 + x)[y] = '~';
+        }
+    }
 
 }
 
@@ -26,18 +61,25 @@ void drawIntro()
            "--------------------------------------------------------------------------------\n");
 }
 
-int getBattlegroundSize() //Fragt den Spiele nach der gewünschten Feldgröße
+void drawScreen(char (*matrix)[], int size, int playerTurn) /*Zeigt den Spieler, der an der Reihe ist, an und zeichnet dessen Matrize*/
 {
-    int n;
-    do{
-        printf("Waehlen sie eine Feldgroesse: (min 10, max 20): ");
-        scanf("%i", &n);
-        if (n < 10 || n > 20){
-            printf("Keine gueltige Eingabe!\n");
-            n = 0;
-        } else {
-            return n;
+    int x, y;
+
+    clearScreen();
+    for(x = 0; x < size; x++){
+        for(y = 0; y < size; y++){
+            printf("%c  ", (*matrix + x)[y]);
         }
-    } while (!n);
+        printf("\n");
+    }
+
+    printf("Spieler %i ist an der Reihe\n", playerTurn);
+
+}
+
+void clearScreen() /*Ein wunderschöner Weg um den Screen sauber zu bekommen*/
+{
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+           "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
 
