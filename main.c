28938a1c54b71a683e8f7fa0b74599e072689e43
Benno <bennington.schaab@gmail.com>
2019-11-26 16:30:54 +0100

    Fertigstellung der Ki 3 (untested)

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ void clearScreen();
 
 /*Fuer die KI*/
 int * ki(char (*matrix)[], int size, int difficulty);
-int is_useful(char (*matrix)[], int y, int x, int difficulty);
+int is_useful(char (*matrix)[], int y, int x, int size, int difficulty);
 int * get_from_solo(char (*matrix)[], int y, int x, int difficulty);
 int * get_from_double(char (*matrix)[], int y, int x, int difficulty);
 
@@ -33,7 +33,6 @@ int main(void) {
     initBattleground(battleground0, battleground1, size); /*TODO: Das hier moechte in die setzeSchiffe Methode*/
     drawScreen(battleground0, size, playerTurn);
 
-    ki(battleground0,size,3);
 
 
     return 0;
@@ -101,7 +100,7 @@ void clearScreen() /*Ein wunderschöner Weg um den Screen sauber zu bekommen*/
 /*--------------KI---------------*/
 
 
-int * ki(char (*matrix)[], int size, int difficulty)
+int * ki_diif3(char (*matrix)[], int size)
 {
 
     static int result[2];
@@ -135,10 +134,8 @@ int * ki(char (*matrix)[], int size, int difficulty)
                     last_hit[1] = ix;
                 }
                 count_hit++;
-            } else if ((*matrix + iy*size)[ix] == 'w' || (*matrix + iy*size)[ix] == 'u'){
-                if (is_useful(matrix, iy, ix, difficulty) == 1){
+            } else if (is_useful(matrix, iy, ix, size, 3) == 1) {
                     count_useful++;
-                }
             }
         }
     }
@@ -148,40 +145,90 @@ int * ki(char (*matrix)[], int size, int difficulty)
         in = 0;
         for (iy = 0; iy < size; iy++) {
             for (ix = 0; ix < size; ix++) {
-                if ((*matrix + iy * size)[ix] == 'w' || (*matrix + iy * size)[ix] == 'u') {
-                    if (is_useful(matrix, iy, ix, difficulty) == 1) {
-                        if (in == random_num) {
-                            result[0] = iy;
-                            result[1] = ix;
-                            return result;
-                        }
+                if (is_useful(matrix, iy, ix, size, 3) == 1) {
+                    if (in == random_num) {
+                        result[0] = iy;
+                        result[1] = ix;
+                        return result;
                     }
                 }
             }
         }
     } else if (count_hit == 1){
-        if (is_useful(matrix, first_hit[0] + 1, first_hit[1] + 0, difficulty)){
+        if (is_useful(matrix, first_hit[0] + 1, first_hit[1] + 0, size, 3)) {
             result[0] = first_hit[0] + 1;
             result[1] = first_hit[1] + 0;
-        } else if (is_useful(matrix, first_hit[0] - 1, first_hit[1] + 0, difficulty)) {
+        } else if (is_useful(matrix, first_hit[0] - 1, first_hit[1] + 0, size, 3)) {
             result[0] = first_hit[0] - 1;
             result[1] = first_hit[1] + 0;
-        } else if (is_useful(matrix, first_hit[0] + 0, first_hit[1] + 1, difficulty)) {
+        } else if (is_useful(matrix, first_hit[0] + 0, first_hit[1] + 1, size, 3)) {
             result[0] = first_hit[0] + 0;
             result[1] = first_hit[1] + 1;
-        } else if (is_useful(matrix, first_hit[0] + 0, first_hit[1] - 1, difficulty)) {
+        } else if (is_useful(matrix, first_hit[0] + 0, first_hit[1] - 1, size, 3)) {
             result[0] = first_hit[0] + 0;
             result[1] = first_hit[1] - 1;
         }
+        return result;
+
     } else if (count_hit > 1){
         vector[0] = last_hit[0] - first_hit[0];
-        vector[1]
-    }
+        vector[1] = last_hit[1] - first_hit[1];
 
-    return result;
+        if (vector[0] != 0) {
+            vector[0] /= abs(vector[0]);
+        }
+        if (vector[1] != 0) {
+            vector[1] /= abs(vector[1]);
+        }
+
+        result[0] = last_hit[0] + vector[0];
+        result[1] = last_hit[1] + vector[1];
+
+        if (is_useful(matrix, result[0], result[1], size, 3)) {
+            return result;
+        } else {
+            result[0] = first_hit[0] - vector[0];
+            result[1] = first_hit[1] - vector[1];
+            return result;
+        }
+    }
 }
 
-int is_useful(char (*matrix)[], int y, int x, int difficulty)
+int is_useful(char (*matrix)[], int y, int x, int size, int difficulty)
 {
+    if (y < 0 || y >= size || x < 0 || x >= size) {
+        return 0;
+    }
+    if (!((*matrix + y*size)[x] == 'u' || (*matrix + y*size)[x] == 'w'))  {
+        return 0;
+    }
+    if (difficulty >= 2) {
+        if (y+1 < size) {
+            if ((*matrix + (y + 1)*size)[(x + 0)] == 'd') {
+                return 0;
+            }
+        }
+        if (y-1 >= 0) {
+            if ((*matrix + (y - 1)*size)[(x + 0)] == 'd') {
+                return 0;
+            }
+        }
+        if (x+1 < size) {
+            if ((*matrix + (y + 0)*size)[(x + 1)] == 'd') {
+                return 0;
+            }
+        }
+        if (x-1 >= 0) {
+            if ((*matrix + (y + 0)*size)[(x - 1)] == 'd') {
+                return 0;
+            }
+        }
+    }
+    if (difficulty >= 3) {
+        if ((y + 1) % 2 != (x + 1) % 2){
+            return 0;
+        }
+    }
     return 1;
 }
+
