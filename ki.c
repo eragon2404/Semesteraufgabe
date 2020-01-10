0d9f03ec1719b3d4a212275ef771b8a4092fc239
Benjamin Schaab <bennington.schaab@gmail.com>
2020-01-10 18:07:46 +0100

    ai diff 1 und 2 implementiert

--- ki.c
+++ ki.c
@@ -8,14 +8,121 @@ int * get_ai_turn(char **battleground, int size, int difficulty, int *ships)
 {
     switch (difficulty) {
         case 1:
-            return NULL;
+            return ai_diff1(size);
         case 2:
-            return NULL;
+            return ai_diff2(battleground, size, ships);
         case 3:
             return ai_diff3(battleground, size, ships);
         default:
-            return 0;
+            return NULL;
     }
+}
+
+int * ai_diff1(int size)
+{
+    static int result[2];
+    result[0] = rand() % size;
+    result[1] = rand() % size;
+    return result;
+}
+
+int * ai_diff2(char **matrix, int size, int *ships)
+{
+
+    static int result[2];
+
+    int iy;
+    int ix;
+
+    int count_useful;
+    int count_hit;
+
+    int first_hit[2];
+    int last_hit[2];
+
+    int random_num;
+    int in;
+
+    int vector[2];
+
+    count_useful = 0;
+    count_hit = 0;
+
+    /*Matrix analysieren*/
+    for (iy = 0; iy < size; iy++){
+        for (ix = 0; ix < size; ix++){
+            if (matrix[iy][ix] == 'X'){
+                if (count_hit == 0){
+                    first_hit[0] = iy;
+                    first_hit[1] = ix;
+                } else {
+                    last_hit[0] = iy;
+                    last_hit[1] = ix;
+                }
+                count_hit++;
+            } else if (is_useful(matrix, iy, ix, size, 2, ships) == 1) {
+                count_useful++;
+            }
+        }
+    }
+    /* Kein getroffenes Schiff -> Zufaelliges Feld im Muster nehmen */
+    if (count_hit == 0){
+        random_num = rand() % count_useful;
+        in = 0;
+        for (iy = 0; iy < size; iy++) {
+            for (ix = 0; ix < size; ix++) {
+                if (is_useful(matrix, iy, ix, size, 2, ships) == 1) {
+                    if (in == random_num) {
+                        result[0] = iy;
+                        result[1] = ix;
+                        return result;
+                    } else{
+                        in++;
+                    }
+                }
+            }
+        }
+        /* Ein getroffenes Schiff - Feld -> sinvolles anliegendes Feld nehmen */
+    } else if (count_hit == 1){
+        if (is_useful(matrix, first_hit[0] + 1, first_hit[1] + 0, size, 2, ships)) {
+            result[0] = first_hit[0] + 1;
+            result[1] = first_hit[1] + 0;
+        } else if (is_useful(matrix, first_hit[0] - 1, first_hit[1] + 0, size, 2, ships)) {
+            result[0] = first_hit[0] - 1;
+            result[1] = first_hit[1] + 0;
+        } else if (is_useful(matrix, first_hit[0] + 0, first_hit[1] + 1, size, 2, ships)) {
+            result[0] = first_hit[0] + 0;
+            result[1] = first_hit[1] + 1;
+        } else if (is_useful(matrix, first_hit[0] + 0, first_hit[1] - 1, size, 2, ships)) {
+            result[0] = first_hit[0] + 0;
+            result[1] = first_hit[1] - 1;
+        }
+        return result;
+
+        /* Zwei getroffenes Schiff - Felder -> sinvolles ende des Schiifs nehmen */
+    } else if (count_hit > 1){
+        vector[0] = last_hit[0] - first_hit[0];
+        vector[1] = last_hit[1] - first_hit[1];
+
+        if (vector[0] != 0) {
+            vector[0] /= abs(vector[0]);
+        }
+        if (vector[1] != 0) {
+            vector[1] /= abs(vector[1]);
+        }
+
+        result[0] = last_hit[0] + vector[0];
+        result[1] = last_hit[1] + vector[1];
+
+        if (is_useful(matrix, result[0], result[1], size, 2, ships)) {
+            return result;
+        } else {
+            result[0] = first_hit[0] - vector[0];
+            result[1] = first_hit[1] - vector[1];
+            return result;
+        }
+    }
+    return NULL;
 }
 
 int * ai_diff3(char **matrix, int size, int *ships)
